959c0fd7f4ea2fffbc01dcec8ba03f88d27f8af9
Eduardo Costa <eduardocastro.jj@gmail.com>
2015-06-16 08:27:00 -0300

    created unit test to 'ServerSettings' class

---
+++ test/Data.Generic.Cache.Tests/Settings/ServerSettingsTest.cs
@@ -0,0 +1,19 @@
+﻿using Data.Generic.Cache.Settings;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Data.Generic.Cache.Tests.Settings
+{
+    [TestFixture]
+    public class ServerSettingsTest
+    {
+        [Test]
+        public void Equals_GivenTwoServerSettingsObjects_TheyMustBeEquals()
+        {
+            var serverSettingsA = new ServerSettings("address", 1234, "password");
+            var serverSettingsB = new ServerSettings("address", 1234, "password");
+
+            serverSettingsA.Should().Be(serverSettingsB);
+        }
+    }
+}
